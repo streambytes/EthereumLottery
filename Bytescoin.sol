62f3afe090a491117acda4ae1e7314b1279e9522
MrSenzo <mrsenzo@ubuntu.homenet.telecomitalia.it>
2021-04-03 00:14:31 +0200

    ERC Contract

---
+++ Bytescoin.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.6.0;
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
+}
+
+
+contract ERC20 is IERC20 {
+
+    string public constant name = "Bytescoin";
+    string public constant symbol = "BYC";
+    uint256 public constant decimals = 8;
+    uint256 public constant initialSupply_ = 1312500 ; //Chosen by Fuliggine
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
+
+    
+
+    using SafeMath for uint256;
+    
+    uint256 totalSupply_;
+
+   constructor() public {
+    totalSupply_ = initialSupply_ * (10 ** decimals);
+    balances[msg.sender] = totalSupply_; // Split totalSupply between owner and the contract.
+    }
+
+    function totalSupply() public override view returns (uint256) {
+    return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address spender, uint256 numTokens) public override returns (bool) {
+        allowed[msg.sender][spender] = numTokens;
+        emit Approval(msg.sender, spender, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public override view returns (uint) {
+        return allowed[owner][spender];
+    }
+
+    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+        
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+
+    // Bet function -- some address sends tokens to the contract and we add them to the pool
+
+    /*function bet(int bet) {
+        pool_address = address(this);
+        transfer(pool_address, bet);
+        return rng();
+    }*/
+
+    // RNG function -- generate random n number and get the average value
+
+    // win function
+
+    // prize function
+}
+
